0ab07b81f914931ef1c30a113ac5133c3c0a41e5
Сайлаубеков Талгат Ерханулы <SaylaubekovT@lcoast.itstep>
2023-09-21 18:03:23 +0600

    last commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useFocusEffect } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {
   Button,
@@ -10,7 +10,9 @@ import {
   View,
 } from "react-native";
 import AppDatabase from "./components/AppDatabase";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwt_decode from "jwt-decode";
 
 const appDatabase = AppDatabase;
 
@@ -19,11 +21,38 @@ const Stack = createNativeStackNavigator();
 const pages = {
   signUpP: "Sign Up",
   signInP: "Sign In",
+  postP: "Posts Page",
 };
 
 const SignUpPage = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [allGood, setAllGood] = useState(false);
+
+  const onPressSignUp = async () => {
+    try {
+      await appDatabase.signUp(username, email, password);
+      setEmail("");
+      setPassword("");
+      setUsername("");
+      setAllGood(true);
+      setTimeout(() => {
+        setAllGood(false);
+      }, 2000);
+    } catch (error) {
+      setEmail("");
+      setPassword("");
+      setUsername("");
+      setErrorMessage(error.message);
+      setHasError(true);
+      setTimeout(() => {
+        setHasError(false);
+      }, 2000);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -41,12 +70,32 @@ const SignUpPage = ({ navigation }) => {
           placeholder="Username"
         />
         <TextInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.signBlock_input}
+          placeholder="Email"
+        />
+        <TextInput
           value={password}
           onChangeText={(text) => setPassword(text)}
           style={styles.signBlock_input}
           placeholder="Password"
         />
-        <Button title="Sign Up" disabled={!username || !password} />
+        <Button
+          title="Sign Up"
+          disabled={!username || !password}
+          onPress={onPressSignUp}
+        />
+        {hasError && (
+          <View style={{ marginTop: 15 }}>
+            <Text style={{ color: "red" }}>{errorMessage}</Text>
+          </View>
+        )}
+        {allGood && (
+          <View style={{ marginTop: 15 }}>
+            <Text style={{ color: "green" }}>You signed up!</Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -55,6 +104,25 @@ const SignUpPage = ({ navigation }) => {
 const SignInPage = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onPressSignIn = async (navigation) => {
+    try {
+      await appDatabase.signIn(username, password);
+      setPassword("");
+      setUsername("");
+      navigation.navigate(pages.postP);
+    } catch (error) {
+      setUsername("");
+      setPassword("");
+      setErrorMessage(error.message);
+      setHasError(true);
+      setTimeout(() => {
+        setHasError(false);
+      }, 2000);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -77,13 +145,47 @@ const SignInPage = ({ navigation }) => {
           style={styles.signBlock_input}
           placeholder="Password"
         />
-        <Button title="Sign In" disabled={!username || !password} />
+        <Button
+          title="Sign In"
+          disabled={!username || !password}
+          onPress={() => onPressSignIn(navigation)}
+        />
+        {hasError && (
+          <View style={{ marginTop: 15 }}>
+            <Text style={{ color: "red" }}>{errorMessage}</Text>
+          </View>
+        )}
       </View>
     </View>
   );
 };
 
+const PostPage = () => {
+  return (
+    <View>
+      <Text>asd</Text>
+    </View>
+  );
+};
+
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        const convertedToken = jwt_decode(token);
+        const duration = convertedToken.duration;
+        const start = convertedToken.start;
+        const gap = (new Date(start) - new Date()) / 1000;
+        if (gap >= duration) {
+          console.log(false);
+        } else {
+          console.log(true);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -94,6 +196,7 @@ export default function App() {
       >
         <Stack.Screen name={pages.signUpP} component={SignUpPage} />
         <Stack.Screen name={pages.signInP} component={SignInPage} />
+        <Stack.Screen name={pages.postP} component={PostPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/AppDatabase.js
+++ components/AppDatabase.js
@@ -1,9 +1,75 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import uuid from "react-native-uuid";
+import sign from "jwt-encode";
+
+// AsyncStorage.clear()
 
 class AppDatabase {
-  async signUp() {
+  async signUp(username, email, password) {
+    const user = {
+      userID: uuid.v4(),
+      username: username,
+      email: email,
+      password: password,
+      posts: [],
+    };
+
     const users = await AsyncStorage.getItem("users");
-    const usersObj = JSON.parse(users);
+    const usersArr = JSON.parse(users);
+    if (users === null) {
+      await AsyncStorage.setItem("users", JSON.stringify([user]));
+    } else {
+      const candidate = usersArr.find((user) => {
+        if (user.email == email || user.username == username) {
+          return user;
+        }
+      });
+
+      if (candidate) {
+        throw new Error("User already exists!");
+      } else {
+        await AsyncStorage.setItem(
+          "users",
+          JSON.stringify([...usersArr, user])
+        );
+      }
+    }
+    console.log(JSON.parse(await AsyncStorage.getItem("users")));
+  }
+
+  async signIn(username, password) {
+    const users = await AsyncStorage.getItem("users");
+    const usersArr = JSON.parse(users);
+    if (users === null) {
+      throw new Error("User doesn't exist!");
+    } else {
+      const candidate = usersArr.find((user) => {
+        if (user.username == username) {
+          return user;
+        }
+      });
+
+      if (candidate) {
+        if (candidate.password === password) {
+          const token = sign(
+            {
+              username: candidate.username,
+              email: candidate.email,
+              userID: candidate.userID,
+              posts: candidate.posts,
+              duration: 10,
+              start: new Date()
+            },
+            "secret"
+          );
+          await AsyncStorage.setItem("token", token);
+        } else {
+          throw new Error("Wrong username or password!");
+        }
+      } else {
+        throw new Error("User doesn't exist!");
+      }
+    }
   }
 }
 
